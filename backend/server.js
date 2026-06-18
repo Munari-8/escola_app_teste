696cee4255f071907d30c43b1d7a232720e862aa
Munari <enzomunaridarocha@gmail.com>
2026-06-17 21:59:57 -0300

    v1

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // Rotas aqui
+app.post('/usuarios/cadastrar', (req, res) => {
+    const { nome, email, senha } = req.body;
+    
+    const sql = `
+        INSERT INTO usuarios
+        (nome, email, senha)
+        VALUES (?, ?, ?)
+    `;
 
+    conexao.query(
+        sql,
+        [nome, email, senha],
+        (erro, resultado) => {
+            if (erro) {
+                return res.status(500).json({
+                    mensagem: 'Erro ao cadastrar usuário'
+                });
+            }
+
+            res.json({
+                mensagem: 'Usuário cadastrado com sucesso!'
+            });
+        }
+    );
+});
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
-});
+})
